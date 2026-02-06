66acdc6febcc1c6468124f3ba4b840262ce00118
Abduloh Raupov <abdulohraupov@gmail.com>
2026-02-06 10:58:09 +0500

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Abdulloh Raupov",
+  title: "Junior Developer",
+  description: "Portfolio website",
+  accentColor: "#1cd848",
+  social: {
+    email: "abdulohraupov@gmail.com",
+    github: "https://github.com/abdulohraupov",
+  },
+  aboutMe: "My name's Abdulloh and I'm 14! I'm a junior developer.",
+  skills: ["Java", "Javascript", "Docker"],
+};
